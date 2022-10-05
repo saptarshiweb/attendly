bc64cdcfd393466f2c8c00d317efd311b26a908a
Saptarshi Mandal <saptarshicoding@gmail.com>
2022-10-05 21:38:00 +0530

    QR Implementation

--- lib/screens/events_page.dart
+++ lib/screens/events_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/menu_widget.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
@@ -10,14 +11,49 @@ class EventPage extends StatefulWidget {
 }
 
 class _EventPageState extends State<EventPage> {
+  String result = 'Welcome';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Upcoming Events'),
-        leading: const MenuWidget(),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Upcoming Events'),
+          leading: const MenuWidget(),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                var res = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SimpleBarcodeScannerPage(),
+                    ));
+                setState(() {
+                  if (res is String) {
+                    result = res;
+                  }
+                });
+              },
+              child: Text(result),
+            ),
+          ],
+        )
+        
+        );
+  }
+
+  Widget event() {
+    return Container(
+      decoration: BoxDecoration(
+
       ),
+      child: Column(
+        children: [
+          
+        ],
+      ),
+
     );
   }
 }
