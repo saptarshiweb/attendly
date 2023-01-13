74ef5df351e94d2c8dbe7193f3d11f74f7c24de7
Saptarshi Mandal <saptarshicoding@gmail.com>
2023-01-13 10:33:13 +0530

    Changes in Ticket

--- lib/screens/qr_scanner/ticket_show.dart
+++ lib/screens/qr_scanner/ticket_show.dart
@@ -6,6 +6,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:fluttericon/font_awesome_icons.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ticket_widget/ticket_widget.dart';
 
 class TicketShow extends StatefulWidget {
@@ -17,6 +18,15 @@ class TicketShow extends StatefulWidget {
 
 class _TicketShowState extends State<TicketShow> {
   late Image myImage;
+  // ignore: non_constant_identifier_names
+  String Username = "";
+
+  getname() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? name = prefs.getString('Name');
+
+    Username = name!;
+  }
 
   @override
   void initState() {
@@ -33,79 +43,85 @@ class _TicketShowState extends State<TicketShow> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: 0,
-      ),
-      body: Container(
-        color: eventbgcolor,
-        child: Padding(
-          padding:
-              const EdgeInsets.only(left: 18, right: 14, top: 20, bottom: 12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              //headline
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: t2, width: 1),
-                          borderRadius: BorderRadius.circular(12)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Icon(
-                          FontAwesome.left_open,
-                          color: t2,
-                          size: 18,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'Event Ticket',
-                    style: TextStyle(
-                        color: t2, fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                              builder: (context) => const HomePage()));
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: t2, width: 1),
-                          borderRadius: BorderRadius.circular(14)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Icon(
-                          FontAwesome.home,
-                          color: t2,
-                          size: 22,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              //ticket widget
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: ticketwidget(),
-              )
-            ],
-          ),
+        appBar: AppBar(
+          elevation: 0,
+          toolbarHeight: 0,
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: getname(),
+          builder: ((context, snapshot) {
+            return Container(
+              color: eventbgcolor,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 18, right: 14, top: 20, bottom: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //headline
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: t2, width: 1),
+                                borderRadius: BorderRadius.circular(12)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Icon(
+                                FontAwesome.left_open,
+                                color: t2,
+                                size: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Event Ticket',
+                          style: TextStyle(
+                              color: t2,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) => const HomePage()));
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: t2, width: 1),
+                                borderRadius: BorderRadius.circular(14)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Icon(
+                                FontAwesome.home,
+                                color: t2,
+                                size: 22,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    //ticket widget
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: ticketwidget(),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+        ));
   }
 
   Widget ticketwidget() {
@@ -188,7 +204,7 @@ class _TicketShowState extends State<TicketShow> {
                     ),
                   ),
                   Text(
-                    'Saptarshi',
+                    Username,
                     style: TextStyle(
                       color: t2,
                       fontWeight: FontWeight.bold,
